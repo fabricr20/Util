2300c02e074f3bcaeca2cddb57aaacb3853f3644
Fabricio Conde Rojas <fabricr20@gmail.com>
2021-05-06 09:59:16 +0200

    actualizacon

---
+++ src/Utilidades/hola.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Usuario
+ *
+ */
+public class hola {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola");
+
+	}
+
+}
